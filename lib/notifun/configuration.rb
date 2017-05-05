258d4a3a299dcf31bbb3860e2061e4cfbf519342
Brett Samson <brettsamson@gmail.com>
2017-05-05 12:33:52 -0500

    Add time zone to config.

--- lib/notifun/configuration.rb
+++ lib/notifun/configuration.rb
@@ -1,5 +1,5 @@
 class Notifun::Configuration
-  attr_accessor :notification_methods, :parent_mailer, :push_notifier, :push_config, :text_notifier, :text_config, :wysiwyg, :admin_parent_controller, :user_parent_controller, :controller_method
+  attr_accessor :notification_methods, :parent_mailer, :push_notifier, :push_config, :text_notifier, :text_config, :wysiwyg, :admin_parent_controller, :user_parent_controller, :controller_method, :time_zone
 
   def initialize
     @notification_methods = []
@@ -12,5 +12,6 @@ class Notifun::Configuration
     @admin_parent_controller = "ActionController::Base"
     @user_parent_controller = "ActionController::Base"
     @controller_method = "current_user"
+    @time_zone = "Etc/UTC"
   end
 end
